76573db8f7bb4cf14f72665c12a99d3d7fb66a89
jeesub.kim <zays.kim@gmail.com>
2021-09-29 23:23:18 +0900

    Adding database connection and read code, testing code in main function

--- db.go
+++ db.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,17 +15,19 @@ import (
 type Database interface {
 	Connect()
 	Create()
-	Read() []map[string]interface{}
+	Read(database string, tableOrCollection string) []map[string]interface{}
 	Update()
 	Delete()
 }
 
 //MongoDB struct that implements Database interface
 type MongoDB struct {
-	ip       string
-	port     int
-	user     string
-	password string
+	ip           string
+	port         int
+	user         string
+	password     string
+	client       *mongo.Client
+	databaseName string
 }
 
 //Connect method of MongoDB
@@ -35,6 +38,7 @@ func (m *MongoDB) Connect() {
 	}
 	uri := fmt.Sprintf("mongodb://%s:%v", m.ip, m.port)
 	clientOptions := options.Client().ApplyURI(uri).SetAuth(credential)
+	// client, err := mongo.NewClient(clientOptions)
 	client, err := mongo.Connect(context.TODO(), clientOptions) //Use context.TODO if you don't know what context has to be delivered
 	if err != nil {
 		log.Fatal(err)
@@ -43,6 +47,9 @@ func (m *MongoDB) Connect() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	m.client = client
+	log.Println("Connection complete!", client)
+
 }
 
 //Create function of MongoDB
@@ -51,8 +58,22 @@ func (m *MongoDB) Create() {
 }
 
 //Read function of MongoDB
-func (m *MongoDB) Read() {
-
+func (m *MongoDB) Read(database string, tableOrCollection string) []map[string]interface{} {
+	collection := m.client.Database(database).Collection(tableOrCollection)
+	cur, err := collection.Find(context.TODO(), bson.D{{}}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var results []map[string]interface{}
+	for cur.Next(context.TODO()) {
+		var val interface{}
+		err := cur.Decode(&val)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, map[string]interface{}{"key": val})
+	}
+	return results
 }
 
 //Update function of MongoDB
--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+import "log"
+
 func main() {
+	//Connect with DB
+
+	var database Database
+	mongo := &MongoDB{ip: "localhost", port: 27017, user: "root", password: "admin"}
+	database = mongo
+	database.Connect()
+	log.Println(database.Read("boilerplate", "user"))
 	router := initRoutes()
 
 	router.Run()
